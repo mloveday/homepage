cd1835e105d1adb1cacc5ba8f156f6b6c80de4b7
miles <miles.loveday@gmail.com>
2019-06-17 11:01:27 +0100

    CV tweaks

--- milesloveday.com/src/Entity/CV/CV.php
+++ milesloveday.com/src/Entity/CV/CV.php
@@ -41,8 +41,9 @@ class CV {
 
     public static function myCv(): CV {
         $employers = [
-            new Employer('07/2017 - present','Self employed','Stay at home dad','Looking after children. In spare time generously over-engineering a custom web project in PHP/Symfony, Typescript/React for a local pub to plan/manage staff rota, store accounting data, track revenue, receipts, labour rates, etc.'),
-            new Employer('08/2014 - 06/2017, 02/2018 (part time contractor)','Softwire','Software Developer','Developer working on various client projects including BBC Music and David Lloyd Leisure'),
+            new Employer('03/2017 - present','Self employed','PT Dev / Stay at home dad','Part time developing a custom web project in PHP/Symfony, Typescript/React for a local pub to plan/manage staff rota, store accounting data, track revenue, receipts, labour rates, etc (<a href="https://dashboard.milesloveday.com">demo</a>, <a href="/dashboard">write up</a>). Also looking after my children.'),
+            new Employer('02/2018','Softwire','Part time contractor','Contractor to help with a support backlog at my previous employer'),
+            new Employer('08/2014 - 06/2017','Softwire','Software Developer','Developer working on various client projects including BBC Music and David Lloyd Leisure'),
             new Employer('01/2013 - 08/2012','Walthamstow Hall, Sevenoaks','Teacher of mathematics','Teaching 11-18 year old students. Included IGCSE, A-level and Pre-U'),
             new Employer('09/2008 - 12/2012','King Edward\'s School, Birmingham','Teacher of mathematics','Teaching 11-18 year old students. Included IGCSE, A-level and IB.'),
             new Employer('09/2005 - 04/2007','Metsec Plc','Project Engineer','Designing and developing software to track issues in manufacturing, in Pascal/SQL Server'),

e1667785cc83310823e16df689032aa913cb4f28
Miles Loveday <miles.loveday@gmail.com>
2020-03-01 13:11:53 +0000

    Add link to admin for logged in users; deploy script updates

--- milesloveday.com/deploy.sh
+++ milesloveday.com/deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-DATE=`date '+%Y-%m-%d-%H%M%S'`
 DEPLOY_DIRECTORY="milesloveday.com"
-CONNECTION="milehpsv@server187.web-hosting.com:$DEPLOY_DIRECTORY"
-PRIVATE_KEY="~/.ssh/id_rsa"
+CONNECTION_AND_DEPLOY_DIRECTORY="milehpsv@server187.web-hosting.com:$DEPLOY_DIRECTORY"
+CONNECTION_AND_PUBLIC_DIRECTORY="milehpsv@server187.web-hosting.com:public_html"
 
 yarn install
 if [[ "$?" -ne "0" ]]; then
@@ -18,10 +17,11 @@ echo "WARNING: build directory needs to be manually uploaded at present"
 yarn build
 
 echo "Copying files to remote host..."
-rsync -rvz --exclude ".env" --exclude ".env.local" --exclude ".git" --exclude ".idea" --exclude "assets" --exclude "infrastructure" --exclude "node_modules" --exclude "vagrant" --exclude "var" --exclude "vendor" ./ $CONNECTION
+rsync -rvz --exclude ".env" --exclude ".env.local" --exclude ".git" --exclude ".idea" --exclude "assets" --exclude "infrastructure" --exclude "node_modules" --exclude "vagrant" --exclude "var" --exclude "vendor" ./ $CONNECTION_AND_DEPLOY_DIRECTORY
+rsync -rvz --exclude ".htaccess" ../public_html $CONNECTION_AND_PUBLIC_DIRECTORY
 
 echo "Running setup on remote host..."
-ssh $CONNECTION "cd $DEPLOY_DIRECTORY ; ./setup.sh"
+ssh $CONNECTION_AND_DEPLOY_DIRECTORY "cd $DEPLOY_DIRECTORY ; ./setup.sh"
 
 #echo "Creating symlink to new directory..."
 #ssh admin@milesloveday.com "ln -nfs $DEPLOY_DIRECTORY/public/ /home/dashboard/public_html"

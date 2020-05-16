4141d9d30a1234c098da82df46a024f49bd5326e
Miles Loveday <miles.loveday@gmail.com>
2020-05-16 11:23:51 +0100

    React form example: add TODOs

--- milesloveday.com/assets/js/ReactForm/ReactFormExample.tsx
+++ milesloveday.com/assets/js/ReactForm/ReactFormExample.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 
 export const ReactFormExample: React.FC = props => {
+  // TODO create mock API model & form model
+  // TODO fake an API call
+  // TODO handle API load state changes - loading, error
+  // TODO show form controls
+  // TODO show validation of form controls
+  // TODO show mock save button & disable when clean || invalid
   return <div>Hello world!</div>;
 };
